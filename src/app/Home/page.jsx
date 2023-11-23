2de750046a47ca20d8aa3c32074c9cd8e517fcca
gabdala17 <gisella.abdala@gmail.com>
2023-11-23 19:27:53 -0300

    delete use

--- src/app/Home/page.jsx
+++ src/app/Home/page.jsx
@@ -1,57 +1,57 @@
 "use client";
 
 import React from "react";
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import Image from "next/image";
 import "./HomeP.css";
 import "animate.css";
 
 
 function page() {
-  const [loopNum, setLoopNum] = useState(0);
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [text, setText] = useState("");
-  const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [index, setIndex] = useState(1);
+  // const [loopNum, setLoopNum] = useState(0);
+  // const [isDeleting, setIsDeleting] = useState(false);
+  // const [text, setText] = useState("");
+  // const [delta, setDelta] = useState(300 - Math.random() * 100);
+  // const [index, setIndex] = useState(1);
   const toRotate = ["Páginas Web", "e-commerce", "¡y Mucho más!"];
   const period = 2000;
 
-  useEffect(() => {
-    let ticker = setInterval(() => {
-      tick();
-    }, delta);
+  // useEffect(() => {
+  //   let ticker = setInterval(() => {
+  //     tick();
+  //   }, delta);
 
-    return () => {
-      clearInterval(ticker);
-    };
-  }, [text]);
+  //   return () => {
+  //     clearInterval(ticker);
+  //   };
+  // }, [text]);
 
-  const tick = () => {
-    let i = loopNum % toRotate.length;
-    let fullText = toRotate[i];
-    let updatedText = isDeleting
-      ? fullText.substring(0, text.length - 1)
-      : fullText.substring(0, text.length + 1);
+  // const tick = () => {
+  //   let i = loopNum % toRotate.length;
+  //   let fullText = toRotate[i];
+  //   let updatedText = isDeleting
+  //     ? fullText.substring(0, text.length - 1)
+  //     : fullText.substring(0, text.length + 1);
 
-    setText(updatedText);
+  //   setText(updatedText);
 
-    if (isDeleting) {
-      setDelta((prevDelta) => prevDelta / 2);
-    }
+  //   if (isDeleting) {
+  //     setDelta((prevDelta) => prevDelta / 2);
+  //   }
 
-    if (!isDeleting && updatedText === fullText) {
-      setIsDeleting(true);
-      setIndex((prevIndex) => prevIndex - 1);
-      setDelta(period);
-    } else if (isDeleting && updatedText === "") {
-      setIsDeleting(false);
-      setLoopNum(loopNum + 1);
-      setIndex(1);
-      setDelta(500);
-    } else {
-      setIndex((prevIndex) => prevIndex + 1);
-    }
-  };
+  //   if (!isDeleting && updatedText === fullText) {
+  //     setIsDeleting(true);
+  //     setIndex((prevIndex) => prevIndex - 1);
+  //     setDelta(period);
+  //   } else if (isDeleting && updatedText === "") {
+  //     setIsDeleting(false);
+  //     setLoopNum(loopNum + 1);
+  //     setIndex(1);
+  //     setDelta(500);
+  //   } else {
+  //     setIndex((prevIndex) => prevIndex + 1);
+  //   }
+  // };
   return (
     <>
       {/* <div className="homepage"></div> */}
@@ -64,13 +64,13 @@ function page() {
             ¡Somos 4B Programming!
           </h2>
           <h2 className="text-4xl text-white text-center font-medium mt-4 md:text-left tittleHome1">
-            <span
+            {/* <span
               className="txt-rotate"
               dataPeriod="1000"
               data-rotate='[ "Páginas Web", "e-commerce", "¡Mucho más!" ]'
               >
               <span className="wrap">{text}</span>
-            </span>
+            </span> */}
            
             {/* Our pizza is always made with fresh ingredients. */}
           </h2>
